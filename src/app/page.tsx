8800a024113bbdf57218771438bed93166e5b1f9
filipenadai <f.nadai05@gmail.com>
2023-11-15 13:03:35 -0300

    feat: add new select component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,21 @@ import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 import { Input } from '@/components/form/input'
-import { Textarea } from '@/components/form/textarea'
+import { Select } from '@/components/form/select'
+
+interface Questions {
+  id: string
+  content: string
+  alternatives?: {
+    id: string
+    description: string
+    questionId: string
+  }[]
+}
 
 export default function Home() {
   const { control } = useForm()
-  const [questions, setQuestions] = useState([])
+  const [questions, setQuestions] = useState<Questions[]>([])
 
   async function submitCanva() {
     console.log('a')
@@ -23,7 +33,6 @@ export default function Home() {
       }),
     })
     const json = await response.json()
-
     setQuestions(json)
   }
 
@@ -32,7 +41,7 @@ export default function Home() {
   }, [])
 
   return (
-    <main className="flex h-auto flex-1 flex-col items-center justify-center bg-white p-24">
+    <main className="flex h-auto flex-1 flex-col items-center justify-center bg-white p-6 md:p-24">
       <form
         onSubmit={submitCanva}
         className="flex h-full w-full max-w-xl flex-col items-center justify-center"
@@ -41,24 +50,40 @@ export default function Home() {
           Faça um Business Canva
         </h1>
         <div className="flex w-full flex-col items-center gap-4">
-          <Input
-            control={control}
-            name="business-type"
-            placeholder="Qual o tipo do seu negócio?"
-          />
-          <Input
-            control={control}
-            name="business-localization"
-            placeholder="Qual a localização (Cidade, estado ou país)?"
-          />
-          <Textarea
-            control={control}
-            name="about-business"
-            placeholder="Descreva seu negócio"
-          />
-          <button className="mt-2 max-w-xs rounded-md border-none bg-blue-500 px-8 py-3 font-bold uppercase text-white transition-all hover:bg-blue-950">
-            Enviar
-          </button>
+          {questions.map(q => {
+            if (q?.alternatives) {
+              return (
+                <Select
+                  control={control}
+                  label={q.content}
+                  name={q.id}
+                  key={q.id}
+                  options={q.alternatives}
+                />
+              )
+            } else {
+              return (
+                <Input
+                  key={q.id}
+                  control={control}
+                  name={q.id}
+                  placeholder={q.content}
+                />
+              )
+            }
+          })}
+          {!questions.length && (
+            <img
+              src="/loading.svg"
+              alt="Carregando conteúdo"
+              className="animate-spin"
+            />
+          )}
+          {questions.length && (
+            <button className="mt-2 max-w-xs rounded-md border-none bg-blue-500 px-8 py-3 font-bold uppercase text-white transition-all hover:bg-blue-950">
+              Enviar
+            </button>
+          )}
         </div>
       </form>
     </main>
---
+++ src/components/form/select/index.tsx
@@ -0,0 +1,41 @@
+import { SelectHTMLAttributes } from 'react'
+import { Control, Controller } from 'react-hook-form'
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  control: Control
+  options: {
+    id: string
+    description: string
+    questionId: string
+  }[]
+  name: string
+  label: string
+}
+
+export function Select({ control, label, name, options }: SelectProps) {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field: { name, onBlur, onChange, value } }) => (
+        <div className="w-full">
+          <p className="mb-2 text-blue-950">{label}</p>
+          <select
+            className="w-full rounded-lg bg-blue-200 px-5 py-4 text-blue-950 transition-all"
+            onChange={onChange}
+            onBlur={onBlur}
+            name={name}
+            value={value}
+          >
+            <option value="">Selecione</option>
+            {options.map(q => (
+              <option key={q.id} value={q.id}>
+                {q.description}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
+    />
+  )
+}
