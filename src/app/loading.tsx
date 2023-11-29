db732c7f5f2d6488916d718e93b22636814ca13d
mlucasdev <lucasmsantos819@gmail.com>
2023-11-29 09:29:36 -0300

    feat: add Loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,13 @@
+const Loading: React.FC = () => {
+  return (
+    <div>
+      <img
+        src="/loading.svg"
+        alt="Carregando conteúdo"
+        className="animate-spin"
+      />
+    </div>
+  )
+}
+
+export default Loading
