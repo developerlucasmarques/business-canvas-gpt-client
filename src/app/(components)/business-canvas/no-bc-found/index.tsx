37753ef10672299e3ed768300d1999e43e1b4399
mlucasdev <lucasmsantos819@gmail.com>
2023-11-29 11:11:52 -0300

    feat: add NoBcFound Component

---
+++ src/app/(components)/business-canvas/no-bc-found/index.tsx
@@ -0,0 +1,7 @@
+export const NoBcFound: React.FC = () => {
+  return (
+    <div>
+      <h1>Nenhum Business Canvas Encontrado</h1>
+    </div>
+  )
+}
--- src/app/(main)/business-canvas/page.tsx
+++ src/app/(main)/business-canvas/page.tsx
@@ -7,6 +7,7 @@ import styles from '@/styles/all-business-canvas.module.css'
 import { type BusinessCanvasSummary } from '@/types/api-responses/business-canvas-summary'
 import { type ErrorReponse } from '@/types/api-responses/error-response'
 import { useEffect, useState } from 'react'
+import { NoBcFound } from '@/app/(components)/business-canvas/no-bc-found'
 
 const BusinessCanvas: React.FC = () => {
   const [businessCanvasList, setBusinessCanvasList] = useState<BusinessCanvasSummary[]>([])
@@ -34,10 +35,17 @@ const BusinessCanvas: React.FC = () => {
 
   return (
     <div className={styles.container}>
-      <h1 >Business Canvas Criados</h1>
-      {businessCanvasList.map((item, index) => (
-        <BcListCard key={index} id={item.id} name={item.name} createdAt={item.createdAt}/>
-      ))}
+      {
+        !businessCanvasList.length
+          ? <NoBcFound/>
+          : <>
+            <h1>Business Canvas Criados</h1>
+            {businessCanvasList.map((item, index) => (
+              <BcListCard key={index} id={item.id} name={item.name} createdAt={item.createdAt}/>
+            ))}
+          </>
+      }
+
       <SendLink label='Criar Novo' url='/'/>
     </div>
   )
