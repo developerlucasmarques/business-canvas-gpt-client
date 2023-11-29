54f876bc84592b53ccb0339d0343821bdd73ad41
mlucasdev <lucasmsantos819@gmail.com>
2023-11-29 11:22:18 -0300

    feat: add color to h1 tag

--- src/app/(components)/not-found/index.tsx
+++ src/app/(components)/not-found/index.tsx
@@ -3,9 +3,12 @@ interface Props {
 }
 
 export const NotFound: React.FC<Props> = ({ value }: Props) => {
+  const titleStyle = {
+    color: 'var(--color-title)'
+  }
   return (
     <div>
-      <h1 className="text-6xl font-bold">
+      <h1 className="text-6xl font-bold" style={titleStyle}>
         {value} não encotrado
       </h1>
     </div>
