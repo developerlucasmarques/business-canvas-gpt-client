d7e735200848002863e06955a103e9ee57863584
mlucasdev <lucasmsantos819@gmail.com>
2023-11-30 20:21:58 -0300

    fix: change name regex

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -5,7 +5,7 @@ import { z } from 'zod'
 const required_error = 'Campo obrigatório'
 
 const SignUpFormSchema = z.object({
-  name: z.string({ required_error }).min(3, 'Nome muito curto').max(50, 'Nome muito grande').regex(/^[a-zA-Z ]+$/, 'Nome não deve conter caracteres especiais ou números'),
+  name: z.string({ required_error }).min(3, 'Nome muito curto').max(50, 'Nome muito grande').regex(/^[a-zA-ZçÇ\s]+$/u, 'Nome não deve conter caracteres especiais ou números'),
   email: z.string({ required_error }).email('Informe um email válido'),
   password: z.string({ required_error }).min(8, 'Sua senha deve conter no mínimo 8 caracteres').max(128).regex(/^(?=.*[a-zA-Z])(?=.*\d).+$/, 'Sua senha deve conter letras e números'),
   passwordConfirmation: z.string({ required_error })
