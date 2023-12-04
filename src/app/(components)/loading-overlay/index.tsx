41aac8b260b9a3cbaba92e13f42adb4685896867
mlucasdev <lucasmsantos819@gmail.com>
2023-12-04 10:21:29 -0300

    feat: add Loading Component to LoadingOverlay

--- src/app/(components)/loading-overlay/index.tsx
+++ src/app/(components)/loading-overlay/index.tsx
@@ -1,3 +1,4 @@
+import Loading from '@/app/loading'
 import styles from './loading-overlay.module.css'
 
 interface Props {
@@ -8,6 +9,7 @@ export const LoadingOverlay: React.FC<Props> = ({ label }: Props) => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Carregando...</h1>
+      <Loading/>
       <p className={styles.label}>{label}</p>
     </div>
   )
