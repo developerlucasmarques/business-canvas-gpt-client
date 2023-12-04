fcae8f4ff0aa3867a9a935f76abcc6f0a471f889
mlucasdev <lucasmsantos819@gmail.com>
2023-12-04 10:18:09 -0300

    refactor: improve code

--- src/app/(components)/business-canvas/bc-container/index.tsx
+++ src/app/(components)/business-canvas/bc-container/index.tsx
@@ -27,55 +27,35 @@ export const BcContainer: React.FC<Props> = ({ businessCanvas }: Props) => {
     keyPartnerships: { title: 'Parcerias-Chaves' },
     costStructure: { title: 'Estrutura de custos' }
   })
-  const is1200 = windowWidth >= 1200
-  const is1000 = windowWidth >= 1000
-  const is680 = windowWidth >= 680
+  const isGreaterThan1200 = windowWidth >= 1200
+  const isGreaterThan1000 = windowWidth >= 1000
+  const isGreaterThan680 = windowWidth >= 680
 
   useEffect(() => {
     const handleResize = (): void => {
       setWindowWidth(window.innerWidth)
     }
     window.addEventListener('resize', handleResize)
-    if (!is1200) {
-      setComponentMapping((prev) => ({
-        ...prev,
-        customerRelationships: {
-          ...prev.customerRelationships,
-          margin: false
-        },
-        keyActivities: {
-          ...prev.keyActivities,
-          margin: false
-        }
-      }))
-    } else {
-      setComponentMapping((prev) => ({
-        ...prev,
-        customerRelationships: {
-          ...prev.customerRelationships,
-          margin: true
-        },
-        keyActivities: {
-          ...prev.keyActivities,
-          margin: true
-        }
-      }))
-    }
+    setComponentMapping((prev) => ({
+      ...prev,
+      customerRelationships: { ...prev.customerRelationships, margin: isGreaterThan1200 },
+      keyActivities: { ...prev.keyActivities, margin: isGreaterThan1200 }
+    }))
     return () => {
       window.removeEventListener('resize', handleResize)
     }
-  }, [is1200])
+  }, [isGreaterThan1200])
 
   const renderColumn = (keys: ComponentName[]): React.ReactNode => (
-    <div className={styles.colum} >
-      {renderCards(keys)}
-    </div>
+    <div className={styles.colum} >{renderCards(keys)}</div>
+  )
+
+  const renderLinesContainer = (keys: ComponentName[]): React.ReactNode => (
+    <div className={styles.businessCanvasLinesContainer}>{renderLine(keys)}</div>
   )
 
   const renderLine = (keys: ComponentName[]): React.ReactNode => (
-    <div className={styles.line} >
-      {renderCards(keys)}
-    </div>
+    <div className={styles.line} >{renderCards(keys)}</div>
   )
 
   const renderCards = (keys: ComponentName[]): React.ReactNode => (
@@ -88,54 +68,27 @@ export const BcContainer: React.FC<Props> = ({ businessCanvas }: Props) => {
       />
     ))
   )
-
+  const keysIsLessThan680 = ['keyPartnerships', 'customerSegments', 'keyActivities', 'keyResources', 'customerRelationships', 'channels', 'revenueStreams', 'costStructure']
   return (
     <div className={`${styles.container} flex justify-center flex-col items-center`}>
       <h1>{businessCanvas.name}</h1>
       <div className={styles.businessCanvasContainer}>
         <div className={styles.businessCanvasColumnsContainer}>
-          {is1000 && renderColumn(['keyPartnerships'])}
-          {is1200 && renderColumn(['keyActivities', 'keyResources'])}
+          {isGreaterThan1000 && renderColumn(['keyPartnerships'])}
+          {isGreaterThan1200 && renderColumn(['keyActivities', 'keyResources'])}
           {renderColumn(['valuePropositions'])}
-          {is1200 && renderColumn(['customerRelationships', 'channels'])}
-          {is1000 && renderColumn(['customerSegments'])}
+          {isGreaterThan1200 && renderColumn(['customerRelationships', 'channels'])}
+          {isGreaterThan1000 && renderColumn(['customerSegments'])}
         </div>
-        <div className={styles.businessCanvasLinesContainer}>
-          {!is680 && renderLine(['keyPartnerships'])}
-        </div>
-        <div className={styles.businessCanvasLinesContainer}>
-          {!is680 && renderLine(['customerSegments'])}
-        </div>
-        <div className={styles.businessCanvasLinesContainer}>
-          {!is680 && renderLine(['keyActivities'])}
-        </div>
-        <div className={styles.businessCanvasLinesContainer}>
-          {!is680 && renderLine(['keyResources'])}
-        </div>
-        <div className={styles.businessCanvasLinesContainer}>
-          {!is680 && renderLine(['customerRelationships'])}
-        </div>
-        <div className={styles.businessCanvasLinesContainer}>
-          {!is680 && renderLine(['channels'])}
-        </div>
-        <div className={styles.businessCanvasLinesContainer}>
-          {!is680 && renderLine(['costStructure'])}
-        </div>
-        <div className={styles.businessCanvasLinesContainer}>
-          {!is680 && renderLine(['revenueStreams'])}
-        </div>
-        <div className={styles.businessCanvasLinesContainer}>
-          {!is1000 && is680 && renderLine(['keyPartnerships', 'customerSegments'])}
-        </div>
-        <div className={styles.businessCanvasLinesContainer}>
-          {!is1200 && is680 && renderLine(['keyActivities', 'keyResources'])}
-        </div>
-        <div className={styles.businessCanvasLinesContainer}>
-          {!is1200 && is680 && renderLine(['customerRelationships', 'channels'])}
-        </div>
-        <div className={styles.businessCanvasLinesContainer}>
-          {is680 && renderLine(['revenueStreams', 'costStructure'])}
-        </div>
+        {!isGreaterThan680 && keysIsLessThan680.map(key => (
+          <div className={styles.businessCanvasLinesContainer}>
+            {renderLine([key as ComponentName])}
+          </div>
+        ))}
+        {!isGreaterThan1000 && isGreaterThan680 && renderLinesContainer(['keyPartnerships', 'customerSegments'])}
+        {!isGreaterThan1200 && isGreaterThan680 && renderLinesContainer(['keyActivities', 'keyResources'])}
+        {!isGreaterThan1200 && isGreaterThan680 && renderLinesContainer(['customerRelationships', 'channels'])}
+        {isGreaterThan680 && renderLinesContainer(['revenueStreams', 'costStructure'])}
       </div>
       <SendLink label='Criar Outro' url='/'/>
     </div>
